21c111f54822966f6c5a3df3c3efeecc8af196b2
Christos Ploutarchou <cploutarchou@gmail.com>
2023-11-11 13:52:23 +0200

    create ticker Data struct

---
+++ parser/binance/structs.go
@@ -0,0 +1,26 @@
+package binance
+
+// TickerData represents the 24-hour ticker data for a trading pair.
+type TickerData struct {
+	AskPrice           string  `json:"askPrice"`
+	AskQuantity        string  `json:"askQty"`
+	BidPrice           string  `json:"bidPrice"`
+	BidQuantity        string  `json:"bidQty"`
+	CloseTime          float64 `json:"closeTime"`
+	TradeCount         int     `json:"count"`
+	FirstTradeID       float64 `json:"firstId"`
+	HighPrice          string  `json:"highPrice"`
+	LastTradeID        float64 `json:"lastId"`
+	LastPrice          string  `json:"lastPrice"`
+	LastQuantity       string  `json:"lastQty"`
+	LowPrice           string  `json:"lowPrice"`
+	OpenPrice          string  `json:"openPrice"`
+	OpenTime           float64 `json:"openTime"`
+	PreviousClosePrice string  `json:"prevClosePrice"`
+	PriceChange        string  `json:"priceChange"`
+	PriceChangePercent string  `json:"priceChangePercent"`
+	QuoteVolume        string  `json:"quoteVolume"`
+	Symbol             string  `json:"symbol"`
+	Volume             string  `json:"volume"`
+	WeightedAvgPrice   string  `json:"weightedAvgPrice"`
+}
