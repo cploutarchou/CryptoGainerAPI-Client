3e478ef6b6f7a070a1d1209eb1c8d651baf69bad
Christos Ploutarchou <cploutarchou@gmail.com>
2023-11-11 13:52:56 +0200

    expose binance package to Parser

--- parser/parser.go
+++ parser/parser.go
@@ -1,4 +1,19 @@
 package parser
 
+import "github.com/cploutarchou/CryptoGainerAPI-Client/parser/binance"
+
 type Parser interface {
+	Binance(apiKey, apiSecret string) *binance.Binance
+}
+
+type parserImp struct {
+}
+
+func NewBinance(apiKey, apiSecret string) *binance.Binance {
+	return binance.New(apiKey, apiSecret)
+
+}
+
+func (p *parserImp) Binance(apiKey, apiSecret string) *binance.Binance {
+	return NewBinance(apiKey, apiSecret)
 }
