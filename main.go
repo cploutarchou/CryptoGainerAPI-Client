b270d80a67769f578a461a7116d884f0df7360fd
Christos Ploutarchou <cploutarchou@gmail.com>
2023-11-11 14:06:29 +0200

    expose parser to main

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
-package CryptoGainerParser
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/cploutarchou/CryptoGainerAPI-Client/parser"
+	"os"
+)
 
 func main() {
+	key := os.Getenv("BINANCE_KEY")
+	secret := os.Getenv("BINANCE_SECRET")
+	cnf := parser.Config{Binance: &parser.Binance{
+		ApiKey:    key,
+		SecretKey: secret,
+	}}
+	parser, _ := parser.New(cnf)
+	tickers, _ := parser.Binance().Get24HourTickerData()
+	data, _ := json.MarshalIndent(tickers, "", "    ")
+	fmt.Println(string(data))
 
 }
